e206d45002ef3e9f443efa74d7594deec8f83f4b
chandantiwari <chandan13tiwaris@gmail.com>
2024-08-18 13:59:07 +0530

    update docker-demo

--- src/test/java/com/poc/docker/controller/PrintControllerTest.java
+++ src/test/java/com/poc/docker/controller/PrintControllerTest.java
@@ -5,18 +5,28 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
+@TestPropertySource("classpath:application-test.properties")
 public class PrintControllerTest {
 
     @InjectMocks
     PrintController printController;
 
+    @Value("${delay-time}")
+    int delayTime;
+
     @Test
     void testAddedTimeDelay() throws InterruptedException {
         // added time delay to slow down mvn clean to showcase docker compose
         String expectedResult = "Process Completed";
-        String actualResult = printController.addedTimeDelay(120000); // delay for 2min
+        System.out.println(delayTime);
+        String actualResult = printController.addedTimeDelay(delayTime); // delay for 2min
 
         Assertions.assertEquals(expectedResult, actualResult);
     }
